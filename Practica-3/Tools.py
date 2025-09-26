88b5a1ac8c58654138a7e79f8b92c48339d2cd57
Bobiptus <pb.armando@gmail.com>
2025-09-26 07:20:48 -0700

    Faltan comentarios y actualizar README

--- Practica-3/Tools.py
+++ Practica-3/Tools.py
@@ -12,10 +12,28 @@ def teleport(x:float, y:float) -> None:
     t.pendown()
 
 def print_dot(x:float, y:float, color:str) -> None:
+    """
+    Funcion para pintar un pixel en las coordenadas indicadas
+    """
     teleport (x,y)
     t.dot(2,color)
 
 def revisa_color(codigo:int) -> str:
+    """"
+    Funcion que se utiliza cuando se desigan el color, este es indicado por el numero que aparece en el arreglo, las opciones son las siguientes:
+
+    0) Blanco
+    1) Negro
+    2) Verde
+    3) Rojo
+    4) Amarillo
+    5) Purpura
+    6) Azul
+    7) Cyan
+    8) Monasterio
+    9) Roja profundo
+
+    """
     paleta = ['white', 'black', 'green', 'red', 'yellow', 'purple', 'blue', 'cyan', 'chartreuse', 'deeppink']
     color_seleccionado = str(paleta[codigo])
     return color_seleccionado
--- Practica-3/practica3_archivo_numericoG.py
+++ Practica-3/practica3_archivo_numericoG.py
@@ -8,27 +8,31 @@ t = turtle.Turtle()
 t.hideturtle()
 t.speed(0)
 
+#Se configura la pantalla
 screen = turtle.Screen()
 screen.title("Mi dibujo con Turtle")
 screen.bgcolor("white")    
-screen.setup(width=100, height=100)  
+screen.setup(width=600, height=600)  
 
-def leer_archivo():
+def main():
+    """
+    Funcion principal que lee el archivo y lo convierte en una cadena para su análisis
+    """
     contador_pixeles = 0 
     global COORDENADA_X, COORDENADA_Y
     filename = "imagen.txt"
     for line in open(filename):
         seq = line.split()
-        if seq:
-            for w in seq[0]:
-                if w and int(w) != 0:
+        if seq:  #Esta condición revisa si aún hay caracteres en el archivo
+            for w in seq[0]: #Esta condición es para saber si no hemos terminado la cadena
+                if w and int(w) != 0: #Esta condición es para no imprimir en color blanco
                     color_pixel = Tools.revisa_color(int(w))
                     Tools.print_dot(COORDENADA_X, COORDENADA_Y, color_pixel)
                 
                 COORDENADA_X = COORDENADA_X + 1
                 contador_pixeles = contador_pixeles + 1
                 
-                if (contador_pixeles == 100):
+                if (contador_pixeles == 100): #Condición para brincar de renglón
                     contador_pixeles = 0
                     COORDENADA_X = 0
                     COORDENADA_Y = COORDENADA_Y - 1
@@ -36,9 +40,9 @@ def leer_archivo():
             return
 
 
-COORDENADA_X = -50  
-COORDENADA_Y = 50   
+COORDENADA_X = -100  
+COORDENADA_Y = 100   
 
-leer_archivo()
+main()
 
 screen.exitonclick()
